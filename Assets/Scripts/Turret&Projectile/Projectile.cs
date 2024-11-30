e13b846bf40763ef559833c475d64d9bf545042e
hadut <76128351+hadut@users.noreply.github.com>
2024-11-30 17:11:31 +0100

    Update Aiming
    [=] Update 360 Aiming
    [+] Add Turret config
    [+] Add Shooting config

--- Assets/Scripts/Turret&Projectile/Projectile.cs
+++ Assets/Scripts/Turret&Projectile/Projectile.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 namespace Turret_Projectile {
     public class Projectile : MonoBehaviour {
-        private float _speed = 5;
+        [SerializeField] private TurretConfig turretConfig;
+        private float _speed; 
         private Rigidbody2D _rb2d;
 
         private void Awake() {
             _rb2d = GetComponent<Rigidbody2D>();
+            _speed = turretConfig.projectileSpeed;
         }
 
         private void Start() {
             _rb2d.velocity = transform.right * _speed;
         }
-    
+
         private void OnTriggerEnter2D(Collider2D other) {
             if (other.gameObject.CompareTag("Ground")) {
                 Destroy(gameObject);
--- Assets/Scripts/Turret&Projectile/ProjectileSpawn.cs
+++ Assets/Scripts/Turret&Projectile/ProjectileSpawn.cs
@@ -1,11 +1,26 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Turret_Projectile {
-    public class ProjectileSpawn: MonoBehaviour {
+    public class ProjectileSpawn : MonoBehaviour {
+        [SerializeField] private AimingSystem aimingSystem;
         [SerializeField] private GameObject projectilePrefab;
+        [SerializeField] private TurretConfig turretConfig;
+        [SerializeField] private GameObject turretSprite;
+        private float _cooldown;
 
-        public void SpawnProjectile(Quaternion angle) {
-            Instantiate(projectilePrefab, transform.position, angle);
+        private void Awake() {
+            _cooldown = turretConfig.shootingCooldown;
+        }
+
+        public IEnumerator SpawnProjectileCoroutine(GameObject target) {
+            while (true) {
+                var angle = aimingSystem.Aim(target);
+                turretSprite.transform.rotation = angle;
+                Instantiate(projectilePrefab, transform.position, angle);
+                yield return new WaitForSeconds(_cooldown);
+            }
         }
     }
 }
