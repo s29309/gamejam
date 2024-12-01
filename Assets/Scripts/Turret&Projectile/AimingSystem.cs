5381fd86ead32a19a7b40e1b27784a6176d2442a
hadut <76128351+hadut@users.noreply.github.com>
2024-12-01 04:56:55 +0100

    change sprite variables names

--- Assets/Scripts/Turret&Projectile/AimingSystem.cs
+++ Assets/Scripts/Turret&Projectile/AimingSystem.cs
@@ -5,20 +5,20 @@ namespace Turret_Projectile {
     public class AimingSystem : MonoBehaviour {
         [SerializeField] private TurretShooting turretShooting;
         [SerializeField] private SpriteRenderer spriteRenderer;
-        [SerializeField] private Sprite spriteA;
-        [SerializeField] private Sprite spriteB;
+        [SerializeField] private Sprite spriteIdle;
+        [SerializeField] private Sprite spriteShooting;
 
         private void OnTriggerEnter2D(Collider2D other) {
             if (other.gameObject.CompareTag("Player")) {
                 turretShooting.SetTarget(other.gameObject);
-                spriteRenderer.sprite = spriteB;
+                spriteRenderer.sprite = spriteShooting;
             }
         }
 
         private void OnTriggerExit2D(Collider2D other) {
             if (other.gameObject.CompareTag("Player")) {
                 turretShooting.SetTarget(null);
-                spriteRenderer.sprite = spriteA;
+                spriteRenderer.sprite = spriteIdle;
             }
         }
 
