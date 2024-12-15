dfe183041ca02c5c0c3f6bd48e31aa498a015339
hadut <76128351+hadut@users.noreply.github.com>
2024-12-15 16:07:43 +0100

    add esc menu

---
+++ Assets/Scripts/EscMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EscMenu : MonoBehaviour {
+    [SerializeField] private GameObject menu;
+
+    private void Awake() {
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            menu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ResumeGameButtonPressed() {
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenuButtonPressed() {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void RestartGameButtonPressed() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
